ec2839507980a774c3293896f2155986d1e835c8
ELIZABETHVELASCO <90348867+ELIZABETHVELASCO@users.noreply.github.com>
2021-10-27 20:55:09 -0500

    Add files via upload

---
+++ estacionamiento.js
@@ -0,0 +1,75 @@
+var Matricula = prompt ("Registre su Matricula");
+
+var Marca = prompt ("¿Cual es la Marca de su Automovil?");
+
+var datos = {
+
+    Matricula: Matricula,
+
+    Marca: Marca,
+
+}
+
+console.log (datos)
+
+
+
+Horas = prompt ("¿Cuantas Horas Estuvo Dentro del Estacionamiento?");
+
+if (Horas = 1 && Horas >= 2 && Horas <=3){
+
+    alert ("La Tarifa es de  $100 Pesos")
+
+}
+
+else if (Horas = 4 && Horas <= 5){
+
+    alert ("La Tarifa es de $200 Pesos")
+
+}
+
+else if (Horas >= 6){
+
+    alert ("La Tarifa es de $500 Pesos" )
+
+}
+
+
+
+{
+
+var Tarifa = prompt ("Ingrese Cantidad Correspondiente");
+
+var x = {
+
+    Cobro: Tarifa,
+
+    }
+
+   
+
+}
+
+
+
+ if (Tarifa <100){
+
+    alert("Verificar Cantidad");
+
+ }
+
+else if (Tarifa >=100){
+
+    alert ("Pago Realizado")
+
+     console.log (x)
+
+}
+
+
+
+alert ("Hay 10 Autos en el Estacionamiento")
+
+Mat = prompt ("Ingrese la Matricula del Automovil que Desea Buscar")
+
+Mar = prompt ("Ingrese la Marca del Automovil que Desea Buscar")
